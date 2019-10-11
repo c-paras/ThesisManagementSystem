9132cfea9083850a61f6883f2a266e527a2f5e3b
rora123 <rora123@DESKTOP-HC8DKVP.localdomain>
2019-10-11 13:59:41 +1100

    Basic db setup

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
   
 from flask import Flask, render_template,session, request, Response, url_for, redirect
 from flask_restful import Resource, Api, reqparse, fields, marshal
+import sqlite3
+
+DATABASE = '/db/thesisManage'
 
 app = Flask(__name__)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def base():
