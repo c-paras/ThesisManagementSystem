3b3b9eb300d1ab784aad6e931fc3a26a9a9ede77
Costa Paraskevopoulos <costa.paras1@gmail.com>
2019-10-17 01:22:57 +1100

    tidy up form validation for register
    use required and area-required
    to handle required fields in forms
    (most fields in our forms will be required,
    so this will serve as a basis template)
    
    also: fix indentation to stick to 2 spaces
    to be consistent with 2 space html

--- static/js/register.js
+++ static/js/register.js
@@ -1,44 +1,53 @@
 function submitRegister() {
-    const email = $('#email');
-    if(email.val().indexOf('@') < 0) {
-        alert('Please enter valid email');
-        return;
+  let invalid = false;
+  $('#registration-form').find('input').each(function () {
+    if ($(this).val() === '') {
+      invalid = true;
     }
-    const pass = $('#password');
-    const confPass = $('#confirm-password');
-    if(pass.val() !== confPass.val()) {
-        alert('Passwords don\'t match');
-        return;
+  });
+  if (invalid || $('#registration-form').find('.invalid').length) {
+    return;
+  }
+
+  const form = $('#registration-form');
+
+  fetch('/register', {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    },
+    method: 'POST',
+    body: form.serialize()
+  })
+  .then(res => res.json())
+  .then(res => {
+    if (res.status === 'fail') {
+      flash(res.message, error = true);
+    } else {
+      flash('Account created!', error = false);
     }
-
-    const form = $('#registration-form');
-
-    fetch('/register', {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json',
-        },
-        method: 'POST',
-        body: form.serialize()
-    })
-        .then(res => res.json())
-        .then(res => {
-            if(res.status === 'fail') {
-                alert('Error: ' + res.message);
-            } else {
-                alert('Success');
-            }
-        });
+  });
 }
 
 function checkPassword() {
-    const pass = $('#password');
-    const confPass = $('#confirm-password');
-    if(pass.val() === confPass.val()) {
-        confPass.removeClass('invalid');
-        confPass.addClass('valid');
-    } else {
-        confPass.removeClass('valid');
-        confPass.addClass('invalid');
-    }
+  const pass = $('#password');
+  const confPass = $('#confirm-password');
+  if (pass.val() === confPass.val()) {
+    confPass.removeClass('invalid');
+    confPass.addClass('valid');
+  } else {
+    confPass.removeClass('valid');
+    confPass.addClass('invalid');
+  }
+}
+
+function checkKey() {
+  const field = $('#registration-key');
+  if (field.val() !== '') {
+    field.removeClass('invalid');
+    field.addClass('valid');
+  } else {
+    field.removeClass('valid');
+    field.addClass('invalid');
+  }
 }
