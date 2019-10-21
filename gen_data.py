44215dfc0b72227899885849950c8ae493ba0257
Alexander Hodges <z5117819@student.unsw.edu.au>
2019-10-21 14:16:20 +1100

    Move topics and courses over to a JSON file
    Also scrape the entire CSE thesis topics db

--- gen_data.py
+++ gen_data.py
@@ -1,6 +1,7 @@
 from app.db_manager import sqliteManager as db
 import bcrypt
 import random
+import json
 
 
 def get_all_types():
@@ -37,96 +38,15 @@ def gen_users():
 
 
 def gen_courses():
-    courses = [('COMP3121', 'Algorithms and Programming Techniques'),
-               ('COMP3131', 'Programming Languages and Compilers'),
-               ('COMP3141', 'Software System Design and Implementation'),
-               ('COMP3151', 'Foundations of Concurrency'),
-               ('COMP3153', 'Algorithmic Verification'),
-               ('COMP3161', 'Concepts of Programming Languages'),
-               ('COMP3211', 'Computer Architecture'),
-               ('COMP3222', 'Digital Circuits and Systems'),
-               ('COMP3231', 'Operating Systems'),
-               ('COMP3311', 'Database Systems'),
-               ('COMP3331', 'Computer Networks and Applications'),
-               ('COMP3411', 'Artificial Intelligence'),
-               ('COMP3421', 'Computer Graphics'),
-               ('COMP3431', 'Robotic Software Architecture'),
-               ('COMP3511', 'Human Computer Interaction'),
-               ('COMP3601', 'Design Project A'),
-               ('COMP3821', 'Extended Algorithms and Programming Techniques'),
-               ('COMP3891', 'Extended Operating Systems'),
-               ('COMP3900', 'Computer Science Project'),
-               ('COMP3901', 'Special Project A'),
-               ('COMP3902', 'Special Project B'),
-               ('COMP4121', 'Advanced and Parallel Algorithms'),
-               ('COMP4128', 'Programming Challenges'),
-               ('COMP4141', 'Theory of Computation'),
-               ('COMP4161', 'Advanced Topics in Software Verification'),
-               ('COMP4336', 'Mobile Data Networking'),
-               ('COMP4337', 'Securing Wireless Networks'),
-               ('COMP4418', 'Knowledge Representation and Reasoning'),
-               ('COMP4511', 'User Interface Design and Construction'),
-               ('COMP4601', 'Design Project B'),
-               ('COMP4920', 'Management and Ethics'),
-               ('COMP4930', 'Thesis Part A'),
-               ('COMP4931', 'Thesis Part B'),
-               ('COMP4941', 'Thesis Part B'),
-               ('COMP4951', 'Research Thesis A'),
-               ('COMP4952', 'Research Thesis B'),
-               ('COMP4953', 'Research Thesis C'),
-               ('COMP4961', 'Computer Science Thesis A'),
-               ('COMP4962', 'Computer Science Thesis B'),
-               ('COMP4963', 'Computer Science Thesis C'),
-               ('COMP6324',
-                'Internet of Things Service Design and Engineering'),
-               ('COMP6441', 'Security Engineering and Cyber Security'),
-               ('COMP6443', 'Web Application Security and Testing'),
-               ('COMP6445', 'Digital Forensics'),
-               ('COMP6447', 'System and Software Security Assessment'),
-               ('COMP6448', 'Security Engineering Masterclass'),
-               ('COMP6451',
-                'Cryptocurrency and Distributed Ledger Technologies'),
-               ('COMP6452',
-                'Software Architecture for Blockchain Applications'),
-               ('COMP6714', 'Information Retrieval and Web Search'),
-               ('COMP6721', '(In-)Formal Methods: The Lost Art'),
-               ('COMP6733', 'Internet of Things Experimental Design Studio'),
-               ('COMP6741', 'Parameterized and Exact Computation'),
-               ('COMP6752', 'Modelling Concurrent Systems'),
-               ('COMP6771', 'Advanced C++ Programming'),
-               ('COMP6841',
-                'Extended Security Engineering and Cyber Security'),
-               ('COMP6843', 'Extended Web Application Security and Testing'),
-               ('COMP6845',
-                'Extended Digital Forensics and Incident Response'),
-               ('COMP9242', 'Advanced Operating Systems'),
-               ('COMP9243', 'Distributed Systems'),
-               ('COMP9301', 'Cyber Security Project'),
-               ('COMP9302', 'Cyber Security Project B'),
-               ('COMP9313', 'Big Data Management'),
-               ('COMP9315', 'Database Systems Implementation'),
-               ('COMP9318', 'Data Warehousing and Data Mining'),
-               ('COMP9319', 'Web Data Compression and Search'),
-               ('COMP9321', 'Data Services Engineering'),
-               ('COMP9322', 'Software Service Design and Engineering'),
-               ('COMP9323', 'Software as a Service Project'),
-               ('COMP9332', 'Network Routing and Switching'),
-               ('COMP9334',
-                'Capacity Planning of Computer Systems and Networks'),
-               ('COMP9417', 'Machine Learning and Data Mining'),
-               ('COMP9418', 'Advanced Topics in Statistical Machine Learning'),
-               ('COMP9444', 'Neural Networks and Deep Learning'),
-               ('COMP9517', 'Computer Vision')]
-    query = []
-    course_codes = []
-    for courseid, name in courses:
-        course_codes.append(courseid)
-        query.append(('courses', [courseid, name], ['code', 'name']))
-    db.insert_multiple(query)
-    return course_codes
+    with open('db/courses.json') as f:
+        courses = json.load(f)
+        query = []
+        for c in courses:
+            query.append(('courses', [c['code'], c['name']], ['code', 'name']))
+            db.insert_multiple(query)
 
 
-def create_topic(name, supervisor, areas):
+def create_topic(name, description, supervisor, areas):
     user_dbid = db.select_columns('users',
                                   ['id'], ['name'], [supervisor])[0][0]
     db.insert_single('topics', [name, user_dbid,
@@ -137,19 +57,19 @@ def create_topic(name, supervisor, areas):
     query = []
     for area in areas:
         query.append(('topic_areas', [area, topic_id], ['name', 'topic']))
-    db.insert_multiple(query)
+    return query
 
 
 def gen_topics(students, supervisors):
-    topics = [
-        ('3D Visualisation of Robot Sensor Data', ['Graphics', 'Robotics']),
-        ('Research and improve the material design language',
-         ['User Interfaces']),
-        ('Improve the Moodle Page', ['User Interfaces'])]
-    random.seed(42)
-    for topic, areas in topics:
-        supervisor = supervisors[random.randrange(0, len(supervisors))]
-        create_topic(topic, supervisor, areas)
+    with open('db/topics.json') as f:
+        topics = json.load(f)
+        random.seed(42)
+        query = []
+        for t in topics:
+            supervisor = supervisors[random.randrange(0, len(supervisors))]
+            query.extend(create_topic(t['name'], t['description'],
+                                      supervisor, t['areas']))
+        db.insert_multiple(query)
 
 
 if __name__ == '__main__':
