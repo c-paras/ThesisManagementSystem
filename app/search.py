056cec049eedcdaa162044ce5fb5e7511fe3ebef
Rora123 <rohanbhat@hotmail.com>
2019-10-23 21:32:27 +1100

    search input fix

--- app/search.py
+++ app/search.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import session
 from flask import request
 from flask import jsonify
+
 import json
 import re
 
@@ -15,45 +16,45 @@ search = Blueprint('search', __name__)
 
 @search.route('/search', methods=['GET', 'POST'])
 @loggedin
-def searchTopic():
+def search_topic():
     if request.method == 'GET':
         return render_template('search.html',
                                heading='Search Topics', title='Search Topics')
 
-    stopWords = ['AND', 'THE', 'WAS', 'IS', 'A', 'WE', 'THAT', 'IN', 'TO']
+    stop_words = ['AND', 'THE', 'WAS', 'IS', 'A', 'WE', 'THAT', 'IN', 'TO']
 
     # getting input from forms
-    searchTopic = list(dict.fromkeys(request.form.getlist('tagsTopic')))
-    searchSuper = list(dict.fromkeys(request.form.getlist('tagsSupervisor')))
-    searchTopic = list(filter(None, searchTopic))
-    searchSuper = list(filter(None, searchSuper))
-    searchTerms = request.form.get('search')
+    search_topic = list(dict.fromkeys(request.form.getlist('tagsTopic')))
+    search_super = list(dict.fromkeys(request.form.getlist('tagsSupervisor')))
+    search_topic = list(filter(None, search_topic))
+    search_super = list(filter(None, search_super))
+    search_terms = request.form.get('search')
     searchCheck = request.form.get('checkbox-vis')
 
     # cleaning up input
-    searchTerms = searchTerms.upper()
-    searchTerms = re.split(r"\s+", str(searchTerms))
-    searchTerms = list(filter(None, searchTerms))
-    searchTerms = [word for word in searchTerms if word not in stopWords]
+    search_terms = search_terms.upper()
+    search_terms = re.split(r"\s+", str(search_terms))
+    search_terms = list(filter(None, search_terms))
+    search_terms = [word for word in search_terms if word not in stop_words]
     db.connect()
 
     # getting submitted topic_area
-    topicAreas = []
-    if len(searchTopic) > 0:
-        for area in searchTopic:
-            topicAreas.append(db.select_columns('topic_areas',
+    topic_area = []
+    if len(search_topic) > 0:
+        for area in search_topic:
+            topic_area.append(db.select_columns('topic_areas',
                                                 ['name', 'topic'],
                                                 ['name'], [area]))
     # getting submitted supervisors
     supervisor = []
-    if len(searchSuper) > 0:
-        for sup in searchSuper:
+    if len(search_super) > 0:
+        for sup in search_super:
             supervisor.append(db.select_columns('users', ['name', 'id'],
                                                 ['name'], [sup]))
 
     # getting rid of empty string and empty results
-    topicAreas = [i for sub in topicAreas for i in sub]
-    topicAreas = [i for i in topicAreas if i != []]
+    topic_area = [i for sub in topic_area for i in sub]
+    topic_area = [i for i in topic_area if i != []]
     supervisor = [i for sub in supervisor for i in sub]
     supervisor = [i for i in supervisor if i != []]
 
@@ -61,28 +62,28 @@ def searchTopic():
     # supervisors if supplied
     # multiple topic_ares and supervisors ar ORed as described in
     # user stories.
-    if len(searchTopic) > 0 and len(topicAreas) == 0:
+    if len(search_topic) > 0 and len(topic_area) == 0:
         return jsonify({'status': 'ok', 'topics': [],
                         'topicsArea': [],
                         'topicSupervisor': []})
-    elif len(searchSuper) > 0 and len(supervisor) == 0:
+    elif len(search_super) > 0 and len(supervisor) == 0:
         return jsonify({'status': 'ok', 'topics': [],
                         'topicsArea': [],
                         'topicSupervisor': []})
 
-    if len(topicAreas) == 0 and len(supervisor) == 0:
+    if len(topic_area) == 0 and len(supervisor) == 0:
         res = db.select_columns('topics',
                                 ['id', 'name', 'supervisor',
                                  'description', 'visible'], None, None)
-    elif len(topicAreas) > 0 and len(supervisor) == 0:
+    elif len(topic_area) > 0 and len(supervisor) == 0:
         temp = []
-        for area in topicAreas:
+        for area in topic_area:
             temp.append(db.select_columns('topics',
                                           ['id', 'name', 'supervisor',
                                            'description', 'visible'],
                                           ['id'], [area[1]]))
         res = [i for sub in temp for i in sub]
-    elif len(topicAreas) == 0 and len(supervisor) > 0:
+    elif len(topic_area) == 0 and len(supervisor) > 0:
         temp = []
         for sup in supervisor:
             temp.append(db.select_columns('topics',
@@ -91,10 +92,10 @@ def searchTopic():
                                           ['supervisor'], [sup[1]]))
         res = [i for sub in temp for i in sub]
 
-    elif len(topicAreas) > 0 and len(supervisor) > 0:
+    elif len(topic_area) > 0 and len(supervisor) > 0:
         temp = []
         for sup in supervisor:
-            for area in topicAreas:
+            for area in topic_area:
                 temp.append(db.select_columns('topics',
                                               ['id', 'name', 'supervisor',
                                                'description', 'visible'],
@@ -104,7 +105,7 @@ def searchTopic():
 
     # checking if search terms are matched
     matched = [False] * len(res)
-    for word in searchTerms:
+    for word in search_terms:
         for i in range(len(res)):
             if (re.search(word, res[i][1].upper())):
                 matched[i] = True
@@ -112,37 +113,37 @@ def searchTopic():
             if (re.search(word, res[i][3].upper())):
                 matched[i] = True
 
-    matchedSearchPhrase = []
-    if len(searchTerms) == 0:
-        matchedSearchPhrase = res
+    matched_search_phrase = []
+    if len(search_terms) == 0:
+        matched_search_phrase = res
     else:
         for i in range(len(res)):
             if (matched[i]):
-                matchedSearchPhrase.append(res[i])
+                matched_search_phrase.append(res[i])
 
     # checking if topics are visible or not
-    toReturnSearches = []
+    to_return_searches = []
     if searchCheck == 'on':
-        for results in matchedSearchPhrase:
+        for results in matched_search_phrase:
             if results[4] == 1:
-                toReturnSearches.append(results)
+                to_return_searches.append(results)
     else:
-        toReturnSearches = matchedSearchPhrase
+        to_return_searches = matched_search_phrase
 
     # getting the topics_areas for the filtered topics
-    toReturnTopicArea = []
-    for topics in toReturnSearches:
-        toReturnTopicArea.append(db.select_columns('topic_areas',
+    to_return_topic_area = []
+    for topics in to_return_searches:
+        to_return_topic_area.append(db.select_columns('topic_areas',
                                                    ['name'],
                                                    ['topic'], [topics[0]]))
 
     # getting the supervisors for the filtered topics
-    toReturnSupervisor = []
-    for topics in toReturnSearches:
-        toReturnSupervisor.append(db.select_columns('users',
+    to_return_supervisor = []
+    for topics in to_return_searches:
+        to_return_supervisor.append(db.select_columns('users',
                                                     ['name'],
                                                     ['id'], [topics[2]]))
 
-    return jsonify({'status': 'ok', 'topics': toReturnSearches,
-                    'topicsArea': toReturnTopicArea,
-                    'topicSupervisor': toReturnSupervisor})
+    return jsonify({'status': 'ok', 'topics': to_return_searches,
+                    'topicsArea': to_return_topic_area,
+                    'topicSupervisor': to_return_supervisor})
