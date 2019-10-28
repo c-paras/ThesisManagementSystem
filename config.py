01b4527d71fb36f6ae164da8d4ac10ce3f34e823
Costa Paraskevopoulos <costa.paras1@gmail.com>
2019-10-29 00:02:21 +1100

    remove unused registration key from config.py
    now all traces of the registration key are
    officially removed from the system - we don't need it
    anymore, and we have a proper public account type now

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ DATABASE = './db/thesis.db'
 
 SECRET_KEY = b'W\xeb\xa7\xc4\x91\xb3\x89\x1e\xb3\xbe'
 
-REGISTRATION_KEY = 'secret'
-
 EMAIL_FORMAT = '^z[0-9]{7}@unsw.edu.au$'
 EMAIL_FORMAT_ERROR = \
     'Expected z5555555@unsw.edu.au<br>where z5555555 is your zID.'
