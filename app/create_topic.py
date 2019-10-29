9989f477bc7bd47fa3fae51293a665f8bd3f0576
Chang Ge <eligechang@gmail.com>
2019-10-29 14:15:50 +1100

    fix my noob bugs that query db before connect

--- app/create_topic.py
+++ app/create_topic.py
@@ -10,6 +10,7 @@ from app.auth import UserRole
 from app.auth import at_least_role
 from app.db_manager import sqliteManager as db
 from app.helpers import error
+from app.queries import queries
 
 create_topic = Blueprint('create_topic', __name__)
 
@@ -30,20 +31,27 @@ def create():
     except ValueError as e:
         return e.args
 
-    # to make sure that "COMP" are upper case
+    # to make sure that "COMP" are upper case and strip for areas and prereqs
     prereqs = [x.upper() for x in prereqs]
-
-    for prereq in prereqs:
-        prereq = prereq.strip()
-        course_id = db.select_columns(
-            'courses', ['id'], ['code'], [prereq]
-        )
-
-        if len(course_id) == 0:
-            return error('Sorry, you have to enter a valid course code!')
+    prereqs = [x.strip() for x in prereqs]
+    areas = [x.strip() for x in areas]
 
     db.connect()
     user_id = session['id']
+
+    # test if there are such course in the database
+    course_ids = []
+    for prereq in prereqs:
+
+        course_id = db.select_columns(
+            'courses', ['id'], ['code'], [prereq]
+        )
+        if len(course_id) == 0:
+            db.close()
+            return error('Sorry, you have to enter a valid course code!')
+        course_ids.append(course_id[0][0])
+
+    # test if there is such topic in the database
     res = db.select_columns('topics', ['name'], ['name'], [topic])
 
     # only check the name of the topic
@@ -51,20 +59,22 @@ def create():
         db.close()
         return error('A topic with that name already exists!')
 
+    # now start to insert data into db
+    # insert topic and get its id
     db.insert_single('topics', [topic, user_id, details],
                      ['name', 'supervisor', 'description'])
+    topic_id = db.select_columns('topics',
+                                 ['id'], ['name'], [topic])[0][0]
 
-    topic_id = db.select_columns('topics', ['id'], ['name'], [topic])[0][0]
-
+    # return error('not yet!')
+    # now get topic areas
     for area in areas:
-        area = area.strip()
-
         # get area_id if area in database
         area_id = db.select_columns('topic_areas',
                                     ['id'],
                                     ['name'],
                                     [area])
-        # else add area to database
+        # else add area to database and get the id
         if len(area_id) == 0:
             db.insert_single('topic_areas',
                              [area],
@@ -79,19 +89,14 @@ def create():
                          [topic_id, area_id[0][0]],
                          ['topic', 'topic_area'])
 
-    for prereq in prereqs:
+    # now insert prereqs
+    for i in range(len(course_ids)):
 
-        prereq = prereq.strip()
-        course_id = db.select_columns(
-            'courses', ['id'], ['code'], [prereq]
+        db.insert_single(
+            'prerequisites',
+            [0, topic_id, course_ids[i]],
+            ['type', 'topic', 'course']
         )
-
-        if len(course_id) != 0:
-            db.insert_single(
-                'prerequisites',
-                [0, topic_id, course_id[0][0]],
-                ['type', 'topic', 'course']
-            )
 
     db.close()
     return jsonify({'status': 'ok'})
