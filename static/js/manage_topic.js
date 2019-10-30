f72cac233fbf4949cd22589e2f6616b57741aaf4
Costa Paraskevopoulos <costa.paras1@gmail.com>
2019-10-30 22:20:22 +1100

    merged with master + removed duplicate function
    note sure why, but the manageTopic function was duplicated...

--- static/js/manage_topic.js
+++ static/js/manage_topic.js
@@ -5,49 +5,23 @@ function submitManage() {
   }
   const data = {};
   $('input[type=checkbox]').each(function () {
-
     const status = $(this).is(':checked');
-    const id = $(this).attr("id");
+    const id = $(this).attr('id');
     data[id] = status;
   });
 
-  makeRequestCustomData('/manage_topic', data, (res) => {
+  makePOSTRequest('/manage_topic', data, (res) => {
     if (res.status === 'fail') {
       flash(res.message, error = true);
     } else {
-      delayToast('Changes Saved!', false);
-      window.location.href = '/home';
-    }
-  });
-}
-
-
-function submitManage() {
-  const form = $('#manage-topic-form');
-  if (!formValid(form)) {
-    return;
-  }
-  const data = {};
-  $('input[type=checkbox]').each(function () {
-
-    const status = $(this).is(':checked');
-    const id = $(this).attr("id");
-    data[id] = status;
-  });
-
-  makeRequestCustomData('/manage_topic', data, (res) => {
-    if (res.status === 'fail') {
-      flash(res.message, error = true);
-    } else {
-      delayToast('Changes Saved!', false);
+      delayToast('Changes saved!', false);
       window.location.href = '/home';
     }
   });
 }
 
 $('#checkall-btn').on('click', function () {
-    const flag = false;
-
-    $('input[type=checkbox]').prop('checked', flag);
-    submitManage();
+  const flag = false;
+  $('input[type=checkbox]').prop('checked', flag);
+  submitManage();
 });
