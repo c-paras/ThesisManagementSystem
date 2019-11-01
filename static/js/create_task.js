5884251cd3f9eae82f77ba6232a7533c5200134a
Costa Paraskevopoulos <costa.paras1@gmail.com>
2019-11-01 22:19:51 +1100

    relabel cloned marking criteria
    id and name attributes have to be relabeled
    upon adding a new criteria - this might be moved
    to helpers.js if needed elsewhere

--- static/js/create_task.js
+++ static/js/create_task.js
@@ -27,13 +27,29 @@ $('[name="marking-method"]').change(function () {
 toggleSubmissionType();
 toggleMarkingMethod();
 
+function relabel(elem, attrs) {
+  let id = 0;
+  for (const attr of attrs) {
+    val = elem.attr(attr);
+    id = parseInt(val.match(/\d+$/)[0]);
+    elem.attr(attr, val.replace(/(\d+)$/, function (all, n) {
+        return parseInt(n) + 1;
+      })
+    );
+  }
+  return id + 1;
+}
+
 function addCriteria() {
   const lastCriteria = $('#criteria-table').find('[name^="marking-criteria-"]:last');
   const newCriteria = lastCriteria.clone();
+  const id = relabel(newCriteria, ['id', 'name']);
   newCriteria.find('input').each(function () {
+    relabel($(this), ['id', 'name']);
     $(this).val('');
     markFieldValid($(this));
   });
+  newCriteria.find('[name="remove-criteria"]').attr('onclick', `removeCriteria(${id})`);
   newCriteria.insertAfter(lastCriteria);
 }
 
