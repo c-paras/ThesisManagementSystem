e6ec9eca5266b94dd7c4a3b571c097acf98e5ceb
Costa Paraskevopoulos <costa.paras1@gmail.com>
2019-11-01 22:28:29 +1100

    load file types from database
    instead of hard-coded the acceepted file types,
    load them from the database table and pass them
    to the template for rendering

--- app/create_task.py
+++ app/create_task.py
@@ -7,6 +7,7 @@ from flask import request
 from app.auth import UserRole
 from app.auth import at_least_role
 from app.helpers import error
+from app.db_manager import sqliteManager as db
 
 create_task = Blueprint('create_task', __name__)
 
@@ -15,8 +16,12 @@ create_task = Blueprint('create_task', __name__)
 @at_least_role(UserRole.COURSE_ADMIN)
 def create():
     if request.method == 'GET':
+        db.connect()
+        file_types = db.select_columns('file_types', ['name'])
+        file_types = list(map(lambda x: x[0], file_types))
+        db.close()
         return render_template('create_task.html', heading='Create Task',
-                               title='Create Task')
+                               title='Create Task', file_types=file_types)
 
     # TODO: post request
     return error('A task with that name already exists!')
