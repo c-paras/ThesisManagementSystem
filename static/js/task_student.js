e79cdecb19bac1a5890991d8b0913cd49448a5e1
Alexander Hodges <z5117819@student.unsw.edu.au>
2019-11-03 17:50:34 +1100

    Add checkbox to certify it is all your own work

--- static/js/task_student.js
+++ static/js/task_student.js
@@ -1,4 +1,8 @@
 function uploadFile() {
+    if($('#all-own-work').prop('checked') !== true) {
+        flash('You must certify it is all your own work', true);
+        return;
+    }
     makeMultiPartRequest('/submit_file_task', $('#file-upload-form'), (res) => {
         if (res.status === 'fail') {
             flash(res.message, true);
