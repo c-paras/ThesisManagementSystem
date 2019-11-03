c413017d86a40e9d97c575ed83651e00bd4a14aa
Alexander Hodges <z5117819@student.unsw.edu.au>
2019-11-03 18:59:03 +1100

    Add spinner to indicate something is happening
    Larger files could take some time to upload, change the submit button
    to a spinner until a response is received

--- static/js/task_student.js
+++ static/js/task_student.js
@@ -1,9 +1,16 @@
-function uploadFile() {
+function uploadFile(btn) {
+    const form = $('#file-upload-form');
     if($('#all-own-work').prop('checked') !== true) {
         flash('You must certify it is all your own work', true);
         return;
     }
-    makeMultiPartRequest('/submit_file_task', $('#file-upload-form'), (res) => {
+    $(btn).parent().children().each(function(index, value) {
+        $(value).toggle();
+    });
+    makeMultiPartRequest('/submit_file_task', form, (res) => {
+        $(btn).parent().children().each(function(index, value) {
+            $(value).toggle();
+        });
         if (res.status === 'fail') {
             flash(res.message, true);
             return;
