a31b8dde309817accb2557b783aa445874f74141
Chang Ge <eligechang@gmail.com>
2019-11-22 21:15:08 +1100

    fix some bugs

--- static/js/search.js
+++ static/js/search.js
@@ -151,16 +151,6 @@ function searchResults() {
   });
 }
 
-function searchResultsPublic() {
-  const form = $('#search-form-public');
-  if (!formValid(form)) {
-    return;
-  }
-  const searchTerm = $('#search-input-public').val();
-  window.location.href = '/search?search=' + searchTerm;
-
-}
-
 function loadPage() {
   makeGETRequest('/search_chips', (res) => {
 
