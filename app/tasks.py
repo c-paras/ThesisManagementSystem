0be399c0e686dde0df3450035e8f4bb9dbe1582a
Rora123 <rohanbhat@hotmail.com>
2019-11-23 18:07:49 +1100

    assessor cannot mark approval tasks

--- app/tasks.py
+++ app/tasks.py
@@ -212,6 +212,12 @@ def mark_task():
         for criteria in task_criteria:
             task_max.append(criteria[3])
 
+        check_if_assessor = db.select_columns('student_topic', ['student'],
+                                              ['assessor', 'student'],
+                                              [session['id'], student_id])
+        is_assessor = False
+        if check_if_assessor:
+            is_assessor = True
         db.close()
 
         heading = f"{task['course_name']} - {task['name']}"
@@ -228,7 +234,8 @@ def mark_task():
                                taskMax=task_max,
                                markedFeedback=marked_feedback,
                                task=task,
-                               marker=True)
+                               marker=True,
+                               isAssessor=is_assessor)
 
     data = json.loads(request.data)
     marks = data['marks']
